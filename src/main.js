68c28480ef24f0d6774acf5dee5c65a4c177eded
Oskar Rough <oskar@rough.dk>
2019-03-01 15:34:57 +0100

    Add galleries

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 const {easing, tween, styler} = window.popmotion
 const {spring, listen, pointer, value} = window.popmotion
+
 const el = document.querySelector('.Spinner')
 const divStyler = styler(el)
 const ballXY = value({x: 0, y: 0}, divStyler.set)
+
+// Rotate it 10 times
 const t = tween({
   from: 0,
   to: {rotate: 180},
@@ -10,16 +13,16 @@ const t = tween({
   ease: easing.backOut,
   flip: Infinity,
   // elapsed: 500,
-  loop: 5
+  loop: 10
   // yoyo: 5
 })
 t.start(divStyler.set)
 
+// Drag and drop effect.
 listen(el, 'mousedown touchstart').start(e => {
   e.preventDefault()
   pointer(ballXY.get()).start(ballXY)
 })
-
 listen(document, 'mouseup touchend').start(() => {
   spring({
     from: ballXY.get(),
@@ -30,3 +33,10 @@ listen(document, 'mouseup touchend').start(() => {
     // damping: 10
   }).start(ballXY)
 })
+
+// Create a flickity gallery from each .Gallery element.
+var galleries = document.querySelectorAll('.Gallery')
+galleries.forEach(el => new Flickity(el, {
+  imagesLoaded: true,
+  lazyLoad: 2 // loads next and previous X slides
+}))
